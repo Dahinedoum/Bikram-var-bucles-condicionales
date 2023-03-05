5a3e52d9a0fef39398f1c0b15f7e2243a82f9d13
NihadMouden <nihadmouden@gmail.com>
2023-03-06 00:04:58 +0100

    Update

---
+++ JS/index.js
@@ -0,0 +1,90 @@
+//### Variables ###
+let variableSinValor;
+let booleano1 = true
+let booleano2 = true
+const PI = 3.14
+const TAU = 2 * PI
+
+//### Booleanos ###
+let booleanoAnd = (booleano1 && booleano2)
+let booleanoNot = !booleano1
+let booleanoMix0 = (booleano1 || booleano2) && (booleano1) || (!booleano1 && !booleano2)
+
+//### Operadores ###
+let incrementarDesp = 2
+let resultadoDesp =  incrementarDesp++
+
+let incrementarAntes = 2
+let resultadoAntes = ++incrementarAntes
+
+//### Bucles ###
+let contarHasta10_2 = 0
+for( contarHasta10_2 = 0; contarHasta10_2 < 10; contarHasta10_2++){
+}
+
+// 11 Preguntar en clase linea 
+// let postI = 0
+// let postJ = 0
+
+// for (let i= 0; i < 11; i++){
+//     postI += postJ++
+// }
+
+
+let sumaPares = 0
+for (let i = 0; i < 10; i++) {
+    if(i % 2 ===0){
+        sumaPares += i
+    }    
+}
+// pour ajouter une valeur a une variable dans une boucles il faut mettre +=
+
+
+//## Iteraciones :european_castle: proyecto individual :european_castle: ##
+//### Variables ###
+
+let variableValorNumerico = 7
+
+const MiNombre = "tu nombre"
+const MiNumeroFav = 9
+
+//### Booleanos ###
+
+let booleanoOr = booleano1 || booleano2
+
+let booleanoMix1 = (booleano1 && (TAU/2 === PI)) || (variableValorNumerico >= MiNumeroFav)
+
+let seisNoEsNueve = (6 !== 9)
+
+let booleanoMix2 = variableValorNumerico > 0 < (MiNumeroFav * TAU)
+// console.log (booleanoMix2)
+
+//### Operadores ###
+
+let valorSuma = MiNumeroFav + variableValorNumerico
+
+let valorResta = MiNumeroFav - variableValorNumerico
+
+let valorMultiplicacion = (MiNumeroFav * variableValorNumerico)
+
+let valorDivision = MiNumeroFav / 3
+
+//### Bucles ###
+
+let contarHasta10 = 0
+ while( contarHasta10 < 10 ){
+    contarHasta10++
+    // console.log(contarHasta10)
+ }
+
+//25 igual que la 11.
+
+let sumaImpares = 0 
+for (let i=0; i < 10; i++){
+    if(i % 2 !== 0){
+        sumaImpares+=i
+    }  
+}
+
+
+ 
